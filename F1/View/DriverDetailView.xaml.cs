15682311b0b28a21583c71ff03a61318f81a3e65
ttk1988 <ttk1988@gmail.com>
2022-11-17 20:48:01 -0300

    Add project files.

---
+++ F1/View/DriverDetailView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using F1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace F1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriverDetailView : ContentPage
+    {
+        public DriverDetailView(Driver driver)
+        {
+            InitializeComponent();
+            BindingContext = driver;
+
+            LabelTeam.Text = driver.Team;
+            LabelCountry.Text = driver.Country;
+        }
+    }
+}
---
+++ F1/View/DriversView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using F1.Data;
+using F1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace F1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriversView : ContentPage
+    {
+        private readonly DriverRepository driverRepository;
+
+        public IList<Driver> ListaDrivers { get; set; }
+        public DriversView()
+        {
+            InitializeComponent();
+            driverRepository = new DriverRepository();
+            ListaDrivers = driverRepository.FindAll();
+
+
+            BindingContext = this;
+            //ListViewDrivers.ItemsSource = listaDrivers;
+
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var driver = (Driver)e.Item;
+            Navigation.PushAsync(new DriverDetailView(driver));
+        }
+
+
+    }
+}
